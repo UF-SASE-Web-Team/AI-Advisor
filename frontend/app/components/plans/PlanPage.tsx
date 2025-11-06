fe025d0f1f5c1d5eccafe93944add0130a3176f6
NivedhaaS <148982596+NivedhaaS@users.noreply.github.com>
2025-11-05 21:06:09 -0500

    updated planpage

--- frontend/app/components/plans/PlanPage.tsx
+++ frontend/app/components/plans/PlanPage.tsx
@@ -2,8 +2,10 @@ import PlanNavigation from "./PlanNavigation";
 
 export default function PlanPage() {
     return (
-        <div>
+        <div className="min-h-screen w-full bg-[#E1EABB] flex flex-col items-start px-8 py-6">
             <PlanNavigation />
+            <h1 className="text-gray-800 text-3xl font-semibold mt-4">Plan page</h1>
+            <div className="w-96 h-24 text-center justify-start text-slate-500 text-4xl font-bold font-['Figma_Hand']">Your Saved Schedules</div>
         </div>
     );
 }
