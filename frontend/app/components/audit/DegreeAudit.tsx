2e3535301261b2a1eabdcd40cb95f07ce3b40b14
kofki <69553679+kofki@users.noreply.github.com>
2025-11-05 19:56:39 -0500

    Setup components

---
+++ frontend/app/components/audit/DegreeAudit.tsx
@@ -0,0 +1,7 @@
+export default function DegreeAudit() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
---
+++ frontend/app/components/plans/PlanNavigation.tsx
@@ -0,0 +1,7 @@
+export default function PlanNavigation() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
