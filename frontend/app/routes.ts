41a7afa2cdc9e02d80d67ebc5164f96ecedce8e8
Ajay Vangara <vangaraajay@gmail.com>
2025-11-05 20:17:27 -0500

    Added routes

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -1,3 +1,10 @@
 import { type RouteConfig, index } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    index("routes/home.tsx"),
+    index("routes/loginPage.tsx"),
+    index("routes/planPages.tsx"),
+    index("routes/mapPage.tsx"),
+    index("routes/chatbotPage.tsx"),
+    index("routes/classesPage.tsx")
+] satisfies RouteConfig;
