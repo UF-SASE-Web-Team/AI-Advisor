31a375d4aa9bf0a55ab47b853539a4085d187a6b
kofki <kenzof28@gmail.com>
2026-02-17 21:43:53 -0500

    idk what i did but it works

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true }
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended'],
+  settings: { react: { version: 'detect' } },
+  env: { browser: true, node: true, es2022: true },
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }]
+  }
+};
