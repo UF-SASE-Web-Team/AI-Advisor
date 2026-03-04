a65cacd9796cec55d74e20a93dad23e0394f53d5
kenneth <kokiidoki@gmail.com>
2026-03-04 18:19:42 -0500

    hook up listeners to effect

--- frontend/app/components/Sidebar.tsx
+++ frontend/app/components/Sidebar.tsx
@@ -1,15 +1,34 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 export function Sidebar({ children }: any) {
   const sidebarRef = useRef<HTMLDivElement>(null);
   const isResizing = useRef(false);
+
+  useEffect(() => {
+    window.addEventListener("mousemove", doResize);
+    // Mouse down must start on the resize bar, but could end anywhere
+    window.addEventListener("mouseup", stopResize);
+
+    return () => {
+      window.removeEventListener("mousemove", doResize);
+      window.removeEventListener("mouseup", stopResize);
+    };
+  });
+
+  const doResize = (e: MouseEvent) => {
+    if (!isResizing.current || !sidebarRef.current) return;
+    const widthRatio = e.clientX / window.innerWidth;
+    if (widthRatio > 0.05) {
+      // sidebarRef.current.style.
+    }
+  };
 
   const startResize = () => {
     isResizing.current = true;
     document.body.style.cursor = "col-resize";
   };
 
-  const endResize = () => {
+  const stopResize = () => {
     isResizing.current = false;
     document.body.style.cursor = "default";
   };
