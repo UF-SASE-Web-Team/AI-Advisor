6e197cff47dbdae5afaebfe7300293098f446958
kenneth <kokiidoki@gmail.com>
2026-03-04 17:55:39 -0500

    resize bar div and listener for resize

--- frontend/app/components/Sidebar.tsx
+++ frontend/app/components/Sidebar.tsx
@@ -1,11 +1,28 @@
+import { useRef } from "react";
+
 export function Sidebar({ children }: any) {
+  const sidebarRef = useRef<HTMLDivElement>(null);
+  const isResizing = useRef(false);
+
+  const startResize = () => {
+    isResizing.current = true;
+    document.body.style.cursor = "col-resize";
+  };
+
+  const endResize = () => {
+    isResizing.current = false;
+    document.body.style.cursor = "default";
+  };
+
   return (
     <div
+      ref={sidebarRef}
       className="
               col-span-1 grid grid-rows-2
               border-r"
     >
       {children}
+      <div onMouseDown={startResize} className=""></div>
     </div>
   );
 }
