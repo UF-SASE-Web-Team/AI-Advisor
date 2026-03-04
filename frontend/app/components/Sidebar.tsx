0e66e99976fc4e618b11e2216dbac9619166a3a9
kenneth <kokiidoki@gmail.com>
2026-03-04 18:54:52 -0500

    set proper bounds on width, use vw instead of px

--- frontend/app/components/Sidebar.tsx
+++ frontend/app/components/Sidebar.tsx
@@ -4,7 +4,7 @@ export function Sidebar({ children }: any) {
   const sidebarRef = useRef<HTMLDivElement>(null);
   const isResizing = useRef(false);
 
-  useEffect(() => {
+  const initWidth = useEffect(() => {
     window.addEventListener("mousemove", doResize);
     // Mouse down must start on the resize bar, but could end anywhere
     window.addEventListener("mouseup", stopResize);
@@ -17,32 +17,42 @@ export function Sidebar({ children }: any) {
 
   const doResize = (e: MouseEvent) => {
     if (!isResizing.current || !sidebarRef.current) return;
-    const widthRatio = e.clientX / window.innerWidth;
-    if (widthRatio > 0.05) {
-      sidebarRef.current.style.width = widthRatio * window.innerWidth + "px";
+    const widthRatio = (e.clientX / window.innerWidth) * 100;
+    if (widthRatio > 5) {
+      sidebarRef.current.style.width = widthRatio + "vw";
     }
   };
 
   const startResize = () => {
     isResizing.current = true;
     document.body.style.cursor = "col-resize";
+    document.body.style.userSelect = "none";
   };
 
   const stopResize = () => {
     isResizing.current = false;
-    document.body.style.cursor = "default";
+    document.body.style.cursor = "auto";
+    document.body.style.userSelect = "auto";
   };
 
   return (
     <div
       ref={sidebarRef}
-      style={{ width: 0.3 * window.innerWidth }}
+      style={{ width: "30vw", minWidth: "20vw", maxWidth: "80vw" }}
       className="
         flex flex-col
-        border-r"
+        border-r
+        relative"
     >
       {children}
-      <div onMouseDown={startResize} className=""></div>
+
+      <div
+        onMouseDown={startResize}
+        className="
+        absolute right-0 top-0 h-full w-1
+        cursor-col-resize
+        "
+      ></div>
     </div>
   );
 }
