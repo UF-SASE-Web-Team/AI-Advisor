22eef2f1095d7fb5bfa19aa882346dd4d45ff465
kenneth <kokiidoki@gmail.com>
2026-04-07 11:30:55 -0400

    Remove broken code with no clear purpose from landing page

--- frontend/app/routes/landingPage.tsx
+++ frontend/app/routes/landingPage.tsx
@@ -1,16 +1,7 @@
-import type { Route } from "./+types/home";
 import { mainLanding } from "../components/landingPage/mainLanding";
 import { AboutLanding } from "../components/landingPage/aboutLanding";
 import { ProfileLanding } from "../components/landingPage/profileLanding";
 import { OtherProjectsLanding } from "../components/landingPage/otherProjectsLanding";
-
-
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
 
 export default function landingPage() {
   return (
