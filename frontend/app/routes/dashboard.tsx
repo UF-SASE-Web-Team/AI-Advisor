f7f905fd00eb18717228a62f610e64181ca9c1bb
kenneth <kokiidoki@gmail.com>
2026-04-08 15:47:16 -0400

    redirect not signed in users from dashboard (google only)

--- frontend/app/routes/dashboard.tsx
+++ frontend/app/routes/dashboard.tsx
@@ -2,9 +2,24 @@ import { DegreeAudit } from "~/components/dashboard/DegreeAudit";
 import { Calendar } from "~/components/dashboard/Calendar";
 import { SelectPlan } from "~/components/dashboard/SelectPlan";
 import { ChatContainer } from "~/components/Chatbot/ChatContainer";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
+import { supabase } from "supabase";
 
 export default function Dashboard() {
-  // TODO: redirect to login page if user isn't logged in
+  // Redirect user if not logged in
+  const navigate = useNavigate();
+  useEffect(() => {
+    const checkAuth = async () => {
+      // TODO: At this point, only google login is implemented and populates the session var
+      // Manual login might need a different check
+      const { data: { session } } = await supabase.auth.getSession();
+      if (!session) { navigate("/login"); }
+    };
+    checkAuth();
+  }, [navigate]);
+
+  // Dashboard page
   return (
     <div className="grid grid-cols-[min-content_1fr] h-screen">
       <div
