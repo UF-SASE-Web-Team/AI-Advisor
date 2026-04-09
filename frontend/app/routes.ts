681ef405261b0e7e1fabfbecc23f81474cd49ee0
Leonna Xie <197978858+koicaaaa@users.noreply.github.com>
2026-04-08 21:16:39 -0400

    Added the transcript widget

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -4,4 +4,5 @@ export default [
   index("routes/landingPage.tsx"),
   route("/login", "routes/loginPage.tsx"),
   route("/dashboard", "routes/dashboard.tsx"),
+  route("/profile", "routes/profile.tsx"),
 ] satisfies RouteConfig;
